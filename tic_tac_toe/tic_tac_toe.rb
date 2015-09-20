d372b65ab81fc863e827f7bdfb330bd32c955dfe
Abner Yang <abtyang@gmail.com>
2015-09-19 22:44:50 -0400

    rearranged game folder structure

---
+++ tic_tac_toe/tic_tac_toe.rb
@@ -0,0 +1,3 @@
+puts "Hi, welcome to a simple terminal Tic-Tac-Toe game. Plase choose whether you'd like to be 'x' or 'o,' please."
+noughtsOrCrosses = gets.chomp.downcase
+puts "Great, thanks for choosing #{noughtsOrCrosses}."
