94f788981ff084b6dbcc7d1e32f4341ec661bf43
Abner Yang <abtyang@gmail.com>
2015-10-12 23:51:41 -0400

    trimmed trailing white space and updated mistake in computer move logic

--- tic_tac_toe/ttt.rb
+++ tic_tac_toe/ttt.rb
@@ -29,8 +29,7 @@ class Game
 	end
 
 	def play_game
-		while @current_turn < @board.layout.length*@board.layout.length do
-			draw_board	
+		while @current_turn < @board.layout.length*@board.layout.length do draw_board
 			next_turn
 			system "clear"
 			end_game?
@@ -44,9 +43,9 @@ class Game
 		i = 0
 		while i < @board.layout.length do
 
-		puts "#{@board.layout[i]}"
+			puts "#{@board.layout[i]}"
 
-		i += 1
+			i += 1
 		end
 	end
 	
@@ -61,12 +60,12 @@ class Game
 		@p1 = Player.new("#{@noughts_or_crosses}", "#{@name}", "hum")
 		if @p1.marker == "x" ? @p2 = Player.new("o", "botbot", "comp") : @p2 = Player.new("x", "botbot", "comp")
 			puts "Great, thanks for choosing #{@noughts_or_crosses}, #{@name}."
-		end		
-	end
+			end
+		end
 
 	def make_move(player)
 		prompt = "Choose an x and y coordinate, please. The format is 'x, y.' For example, '1, 0'"
-		invalid_prompt = "Try again, invalid move!" 
+		invalid_prompt = "Try again, invalid move!"
 		if player.type == "hum"
 
 			puts prompt
@@ -74,13 +73,12 @@ class Game
 			while move = gets.chomp.split(",") do
 				move.each {|coord| coord.gsub!(/\s+/, "")}
 
-				x_coord = move[0].to_i 
+				x_coord = move[0].to_i
 				y_coord = move[1].to_i
-				
 				if x_coord.between?(0, @board.layout.length) && y_coord.between?(0, @board.layout.length) && @board.layout[x_coord][y_coord] == ""
 					@board.layout[x_coord][y_coord] = player.marker
 					break
-				else 
+				else
 					puts invalid_prompt
 				end
 			end
@@ -88,24 +86,24 @@ class Game
 			# simulates 'un-smart' computer move
 
 			loop do
-			x_coord = rand(0..2)
-			y_coord = rand(0..2)
+				x_coord = rand(0..@board.layout.length)
+				y_coord = rand(0..@board.layout.length)
 
-			break if @board.layout[x_coord][y_coord] == "" 
+				break if @board.layout[x_coord][y_coord] == "" 
 			end
 			@board.layout[x_coord][y_coord] = player.marker
 
 		end
-			check_for_winner(player)
+		check_for_winner(player)
 	end
 
 	def next_turn
 		@current_turn += 1
-		@current_turn.odd? ? make_move(@p1) : make_move(@p2) 	
+		@current_turn.odd? ? make_move(@p1) : make_move(@p2)
 	end
 
 	def check_for_winner(player)
-		 if check_horiz(player) || check_vert(player)|| check_diag1(player) || check_diag2(player)
+		if check_horiz(player) || check_vert(player)|| check_diag1(player) || check_diag2(player)
 			@winner = player.name
 		end
 	end
@@ -115,13 +113,13 @@ class Game
 			puts "Good job, #{@winner}; you're a real winner!"
 			puts "Do you want to play again? Yes or no?"
 			answer = gets.chomp.downcase
-				if answer == "yes"
-					g = Game.new
-					g.setup_game
-				else
-					puts "Thanks for playing!"
-					exit
-				end
+			if answer == "yes"
+				g = Game.new
+				g.setup_game
+			else
+				puts "Thanks for playing!"
+				exit
+			end
 		end
 	end
 
@@ -131,7 +129,7 @@ class Game
 			if @board.layout[i].uniq == [player.marker]
 				return true
 			end
-		i += 1
+			i += 1
 		end
 	end
 
@@ -142,7 +140,7 @@ class Game
 			if v.uniq == [player.marker]
 				return true
 			end
-		i += 1
+			i += 1
 		end
 	end
 
